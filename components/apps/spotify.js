4c039fc081558dddf4c7d2bbcdaa23748aa0d856
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-10-30 16:58:00 -0400

    add some games

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function Spotify() {
     return (
-        <iframe src="https://open.spotify.com/embed/playlist/0wJVn9OQoNuRlSHbzzoMai" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://www.friv4school.io/g/angry-shooter" width="800" height="600" scrolling="none" frameborder="0"></iframe>
+        // <iframe className="h-full w-full bg-ub-cool-grey" src="https://previews.customer.envatousercontent.com/files/284246327/index.html" allowfullscreen></iframe>
+        // <iframe src="https://open.spotify.com/embed/playlist/0wJVn9OQoNuRlSHbzzoMai" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
